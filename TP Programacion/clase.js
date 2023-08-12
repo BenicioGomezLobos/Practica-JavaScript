f060f03c71722aac40c19bddc8afff74e4747f60
BenicioGomezLobos <136343454+BenicioGomezLobos@users.noreply.github.com>
2023-08-12 12:53:38 -0300

    Add files via upload

---
+++ TP Programacion/clase.js
@@ -0,0 +1,17 @@
+alert('Hola a todos')
+console.log('la suma es igual a' , 40+20)
+console.log('la resta es igual a', 50-20)
+console.log('la multiplicacion es igual a', 4*5)
+console.log('la divicion es igual a', 5/2)
+let suma = 20 + 10
+alert('el resultado de la suma es:' + suma)
+let nombreAlumno
+nombreAlumno=prompt('Ingresa tu nombre')
+alert('Bienvenido ' + nombreAlumno)
+alert('mi edad es 14')
+apellidoAlumno=prompt('ingresa tu apellido')
+alert('Bienvenido' + nombreAlumno + apellidoAlumno)
+console.log('el resultado es igual a', 10)
+console.log('el resultado es igual a', 3)
+alert('resultado de la multiplicacion = 30')
+
